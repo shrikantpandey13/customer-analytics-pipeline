95ff53d60bb01cc5a82b7839fbcd5a7833e141ae
shrikantpandey13 <shrikantpandeymnnit2015@gmail.com>
2023-10-02 14:59:42 +0530

    Added data extraction script

---
+++ extraction_script.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect('customer_data.db')
+# Extract data
+query = "SELECT * FROM customers"
+data = pd.read_sql(query, conn)
+# Save data to CSV
+data.to_csv('customer_data.csv', index=False)
+# Close the database connection
+conn.close()
